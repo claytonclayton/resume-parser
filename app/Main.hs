ece7be2411070deac43d5f886abd89df9bac113d
Ashley Clayton <ashman2600@gmail.com>
2025-12-16 17:40:22 +1100

    successfully parses intro, section and block

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,11 @@
+
 module Main (main) where
 
+import Text.Megaparsec (parseTest)
+import Lib (parseResume)
+import qualified Data.Text.IO as T
+
 main :: IO ()
-main = putStrLn "cat"
+main = do
+  md <- T.readFile "resume.md"
+  parseTest parseResume md
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 
-module Lib (Resume, Intro, Block, Flat, Line, Parser) where
+module Lib (Resume, Intro, Block, Flat, Line, Parser, parseResume, parseSep) where
 
+import Control.Monad (when) 
 import Data.Char (isAlphaNum)
 import Data.Text (Text)
 import Data.Void
 import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char
 import qualified Data.Text as T
+import Control.Monad (void)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -25,11 +27,16 @@ data Intro = Intro
   }
   deriving (Eq, Show)
 
+data BlockTraits = BlockTraits
+  { topRight :: Text
+  , botLeft  :: Text
+  , botRight :: Text
+  }
+  deriving (Eq, Show)
+
 data Block = Block
-  { blockTitle :: Text
-  , topRight   :: Maybe Text
-  , botLeft    :: Maybe Text 
-  , botRight   :: Maybe Text
+  { blockTitle  :: Text
+  , blockTraits :: Maybe BlockTraits
   }
   deriving (Eq, Show)
 
@@ -50,15 +57,69 @@ data Line = S Section | B Block | D Dot | F Flat
 
 type Parser = Parsec Void Text
 
+parseChars :: Parser Text
+parseChars = 
+  takeWhile1P Nothing (/= '\n') 
+
 parseNot :: Char -> Parser Text
 parseNot c = 
   takeWhileP Nothing (\x -> x /= c && x /= '\n') 
 
-parseLine :: Parser Text
-parseLine = 
-  takeWhile1P Nothing (/= '\n') 
-
 parseSep :: Char -> Parser [Text]
 parseSep c = 
-  (fmap . fmap) T.strip $ (parseNot '|') `sepBy` char c 
+  (fmap . fmap) T.strip $ (parseNot c) `sepBy` char c 
+
+parseIntro :: Parser Intro
+parseIntro = do 
+  title  <- char '#' *> hspace *> parseChars
+  newline
+  traits <- parseSep '|'
+  return Intro {..}
+ 
+parseSection :: Parser Section
+parseSection =
+  fmap Section $ string "##" *> hspace *> parseChars
+
+parseDot :: Parser Dot
+parseDot = 
+  fmap Dot $ char '-' *> hspace *> parseChars
+
+parseBlockTraits :: Parser (Maybe BlockTraits)
+parseBlockTraits =
+  optional $ do
+    string "+ "
+    line <- parseSep '|'
+    when (length line /= 3) $ do
+      fail "blockTraits requires two '|'"
+    let topRight = line !! 0 
+        botLeft  = line !! 1
+        botRight = line !! 2
+    return BlockTraits {..}
+
+parseBlock :: Parser Block
+parseBlock = do
+  blockTitle  <- string "###" *> hspace *> parseChars 
+  newline
+  blockTraits <- parseBlockTraits
+  return Block {..}
+
+parseLine :: Parser Line
+parseLine = do
+  section <- parseSection
+  return $ S section
+
+parseDot :: Parser Dot
+parseDot = undefined
+
+-- many "\n" inefficient?
+parseResume :: Parser Resume
+parseResume = do
+  intro <- parseIntro
+  many "\n"
+  sec   <- fmap S parseSection
+  newline
+  block <- fmap B parseBlock  
+  let lines = [sec, block]
+  return Resume {..}
+
 
