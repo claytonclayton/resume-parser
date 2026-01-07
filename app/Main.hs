66c2ee55f44139f3a2b3049be0ac98f6434ebff2
Ashley Clayton <ashman2600@gmail.com>
2026-01-07 18:26:44 +1100

    implemented escaper in Main that escapes special latex characters

--- app/Main.hs
+++ app/Main.hs
@@ -5,16 +5,40 @@ import Text.Megaparsec (runParser, errorBundlePretty)
 import ResumeParser  (parseResume)
 import ResumeGrouper (groupResumes, ResumeGroup)
 import ResumeGenerator (generateResumes, printResumes)
+import System.Environment (getArgs)
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T
+import qualified Data.Text.Lazy.Builder as B
+import Data.Text.Lazy (toStrict)
+import Data.Text (Text)
 
-inFileName = "me/in/resume.md"
+defaultInFileName = "me/pipeline/md/resume.md"
+
+-- Move to another file
+-- escaper is NAIVE
+-- should check whether the escapees are already escaped
+-- or not before escaping
+linter :: Text -> Text
+linter = toStrict . B.toLazyText . T.foldr escaper mempty . stripper
+  where
+    stripper = T.unlines . fmap T.strip . T.lines
+    escaper c
+      | c `elem` escapees = (<>) $ B.singleton '\\' <> B.singleton c 
+      | otherwise         = (<>) $ B.singleton c
+    escapees = ['&', '$']
 
 main :: IO ()
 main = do
-  md <- TIO.readFile inFileName
-  let lint     = T.unlines $ fmap T.strip $ T.lines md
-      parseOut = runParser parseResume "" lint
+  args <- getArgs
+  let inFileName 
+        = case args of
+            []   -> defaultInFileName
+            x:xs -> x 
+
+  md   <- TIO.readFile inFileName
+
+  let parseOut = runParser parseResume "" $ linter md 
+
   case parseOut of 
     Left  bundle -> putStr $ errorBundlePretty bundle
     Right adt    -> 
