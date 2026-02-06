5faf0696fe257b4bd674046ad27868eb93a80830
Ashley Clayton <ashman2600@gmail.com>
2026-02-06 20:00:21 +1100

    small change to gen.sh

--- me/scripts/gen.sh
+++ me/scripts/gen.sh
@@ -2,11 +2,13 @@
 
 exec_name="gen"
 pdf_path="me/pipeline/pdf/resume.pdf"
+aux="me/aux"
 
 if [ $# -ne 0 ]; then
   arg="$1"
 fi
 
+mkdir -p $aux
 stack build
 stack exec $exec_name $arg
 open $pdf_path
